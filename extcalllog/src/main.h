8e2015454bace2b0ae8617ee0b5d3b272ff06dd9
matrim <ttroy50@gmail.com>
2010-02-18 22:44:47 +0000

    updated to v0.2

--- extcalllog/src/main.h
+++ extcalllog/src/main.h
@@ -48,7 +48,7 @@ G_BEGIN_DECLS
  * Please do not modify "APP_NAME" (or other defines) to different name
  */
 #define APP_NAME "extcalllog"
-#define APP_VER "0.1"
+#define APP_VER "0.2"
 #define APP_SERVICE "org.maemo.extcalllog"
 #define APP_METHOD "/org/maemo/extcalllog"
 /* end defines */
